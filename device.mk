1532d4eb2a6e1e8aa6a2dcf5501a41ba502cba31
dianlujitao <dianlujitao@lineageos.org>
2018-03-13 13:14:01 +0800

    jason: Add missing libnfc-nxp_default.conf
    Change-Id: I3fdcd1093bd850dbd02709b60c09a6ed556718f1

--- device.mk
+++ device.mk
@@ -297,6 +297,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nxp_default.conf:system/etc/libnfc-nxp_default.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf
 
 # OMX
